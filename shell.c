6493403ce67c71cfa44489b13af00c958616db4f
SaurabhSahu01 <saurabhsahu794@gmail.com>
2022-11-18 14:39:19 +0530

    path resolved

--- shell.c
+++ shell.c
@@ -6,9 +6,9 @@
 #include<string.h>
 #include<dirent.h>
 char** split_the_line(char*);
-int stringlength(char* string);
+int stringlength(char*);
 int findlastposition(char, char*);
-char* getDirName(char* path);
+char* getDirName(char* , char*);
 
 int main(){
 	// getting the current working directory and saving its path for future reference
@@ -24,10 +24,13 @@ int main(){
     char command[100];
     char cwd[100];
     do{
+    	char* finaldirectory = (char *)malloc(50);
+    	getcwd(cwd, 100);
         printf("\033[1;32m%s-virtualterminal: ",getenv("USER"));
-        printf("\033[0;34m%s ", getDirName(getcwd(cwd, 100)));
-        printf("\033[1;0m");
+        printf("\033[1;34m%s ", getDirName(cwd, finaldirectory));
+        printf("\033[0;0m");
         printf("$ ");
+        free(finaldirectory);
         fflush(stdin);
         scanf("%[^\n]%*c", command);
 
@@ -184,20 +187,29 @@ int stringlength(char* string){
 // function to find the last position of the character provided by the user
 int findlastposition(char ch, char* string){
     int i=0,j=0;
+    int count = 0;
     while(string[i] != '\0'){
         if(string[i] == ch){
+            count++;
             j = i;
         }
         i++;
     }
-    return j;
+    int returnvalue;
+    if(count > 1 ){
+    	returnvalue = j;
+    }
+    else{
+    	returnvalue = -1;
+    }
+    return returnvalue;
 }
 
 // function to get the directory name of the current working directory
-char* getDirName(char* path){
+char* getDirName(char* path, char dirname[]){
     int length = stringlength(path);
     int position = findlastposition('/', path);
-    char* dirname;
+    if(position == 0 ) dirname = "/";
     strncpy(dirname, path + position + 1, length - position);
     return dirname;
 }
